12702df7fc439c6d730e9210da8771e2181afb7c
haruocode <takaron0930@gmail.com>
2024-01-01 01:13:34 +0900

    add: toggle button

--- sec4/app.vue
+++ sec4/app.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="flex items-center p-10 bg-slate-900" style="height: 100vh;">
-    <span class="text-6xl text-orange-500">Can you see this?</span>
-    <!-- <span class="text-6xl text-slate-600">Falsy</span> -->
+  <div class="p-10 bg-slate-900" style="height: 100vh;">
+    <button class="w-full border-2 border-solid rounded p-4 text-4xl text-slate-300">Toggle</button>
+    <div class="flex flex-col items-center p-10 bg-slate-900" style="height: 100vh;">
+      <span class="text-6xl text-orange-500">Can you see this?</span>
+      <!-- <span class="text-6xl text-slate-600">Falsy</span> -->
+    </div>
   </div>
 </template>
+
+<script setup>
+</script>
