6e18bd2cd34bf4471adc27ee7131e07756227d2c
haruocode <takaron0930@gmail.com>
2023-12-31 20:51:23 +0900

    sec4 ready

--- sec4/app.vue
+++ sec4/app.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="flex items-center p-10 bg-slate-900" style="height: 100vh;">
+    <span class="text-6xl text-orange-500">Can you see this?</span>
+    <!-- <span class="text-6xl text-slate-600">Falsy</span> -->
   </div>
 </template>
