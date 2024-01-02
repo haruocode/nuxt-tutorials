d773577303c14c6d1559a9adfa2abbe20dc4b7a1
haruocode <takaron0930@gmail.com>
2024-01-02 23:26:51 +0900

    sec5 always dark mode

--- sec5/app.vue
+++ sec5/app.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="p-5 bg-white bg-gray-900 antialiased" style="height: 100vh">
     <div
-      class="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+      class="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 bg-gray-800 border-gray-700">
       <div class="flex items-center justify-between mb-4">
-        <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">
+        <h5 class="text-xl font-bold leading-none text-gray-900 text-white">
           Mysterious Customers
         </h5>
       </div>
 
       <div class="flow-root">
-        <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+        <ul role="list" class="divide-y divide-gray-200 divide-gray-700">
           <li class="py-3 sm:py-4">
             <div class="flex items-center">
               <div class="flex-shrink-0">
@@ -17,14 +17,14 @@
                   src="https://pbs.twimg.com/profile_images/1057083258997956608/NWYuC6qb_400x400.jpg" alt="Neil image" />
               </div>
               <div class="flex-1 min-w-0 ms-4">
-                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                <p class="text-sm font-medium text-gray-900 truncate text-white">
                   大阪 太郎
                 </p>
-                <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                <p class="text-sm text-gray-500 truncate text-gray-400">
                   taro.osaka@windster.com
                 </p>
               </div>
-              <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+              <div class="inline-flex items-center text-base font-semibold text-gray-900 text-white">
                 $5,729
               </div>
             </div>
