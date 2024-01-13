54b1ef511461c763ed9c90b520af477ee2fdc596
haruocode <takaron0930@gmail.com>
2024-01-14 06:29:14 +0900

    sec9 ボタンの背景色変更 & v-model消し忘れ対応

--- sec9/pages/create-user.vue
+++ sec9/pages/create-user.vue
@@ -64,7 +64,7 @@
 
         <div class="row">
           <label>自己紹介</label>
-          <textarea rows="5" placeholder="ポン吉って呼んでください。よろしくお願いします！" v-model="user.detail"></textarea>
+          <textarea rows="5" placeholder="ポン吉って呼んでください。よろしくお願いします！"></textarea>
           <input type="checkbox" /><label class="checkbox-label"> 自己紹介を公開する</label>
         </div>
 
@@ -88,7 +88,7 @@
 
         <div v-if="user.public" class="label">※ あなたの自己紹介は公開されます。</div>
 
-        <button @click="register()" type="button" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+        <button @click="register()" type="button" class="w-full text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
           登録
         </button>
 
