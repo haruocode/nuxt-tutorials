aa11f3b4231c0fb4bccec459e3a60f58fce18e2c
haruocode <takaron0930@gmail.com>
2024-01-14 01:18:31 +0900

    sec9 フォームバインディングの基本

---
+++ sec9/pages/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="bg-gray-900 p-3">
+    <AppHeader />
+    <ContentsContainer>
+      <FormContainer title="フォームバインディング">
+
+        <h2>テキストフォーム</h2>
+        <p>textMessage is: {{ textMessage }}</p>
+        <input placeholder="edit me" />
+
+        <hr />
+
+        <h2>複数行テキスト(textarea)</h2>
+        <span>textareaMessage is:</span>
+        <p style="white-space: pre-line;">{{ textareaMessage }}</p>
+        <textarea placeholder="add multiple lines"></textarea>
+
+        <hr />
+
+        <h2>チェックボックス</h2>
+        <input type="checkbox" />
+        <p>checked is: {{ checked }}</p>
+
+        <hr />
+
+        <h2>複数チェックボックス</h2>
+        <p>CheckedNames is: {{ checkedNames }}</p>
+
+        <input type="checkbox" value="吉田">
+        <label for="吉田">吉田</label><br/>
+
+        <input type="checkbox" value="山田">
+        <label for="山田">山田</label><br/>
+
+        <input type="checkbox" value="鈴木">
+        <label for="鈴木">鈴木</label>
+
+        <hr />
+
+        <h2>ラジオボタン</h2>
+        <p>radioPicked is: {{ radioPicked }}</p>
+
+        <input type="radio" value="apple" />
+        <label> 🍎</label><br/>
+
+        <input type="radio" value="orange" />
+        <label> 🍊</label>
+
+      </FormContainer>
+    </ContentsContainer>
+  </div>
+</template>
+
+<script setup>
+
+const textMessage = ref('') // テキスト入力
+const textareaMessage = ref('') // テキストエリア
+const checked = ref(null) // チェックボックス
+const checkedNames = ref([]) // 複数チェックボックス
+const radioPicked = ref('') // ラジオボタン
+
+</script>
+
+<style>
+h2, p, span, label {
+  color: white;
+}
+hr {
+  margin: 8px;
+}
+</style>
