1bb40a01a3c1f47ba93cd90ecad55663ad4f9899
haruocode <takaron0930@gmail.com>
2024-01-13 17:02:32 +0900

    sec9 add コンポーネント

---
+++ sec9/components/App/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="flex items-center gap-4">
+    <Logo />
+    <div class="flex gap-4"></div>
+  </header>
+</template>
+
+<script setup>
+defineProps(['existSelectedCustomer'])
+defineEmits(['deleteCustomer', 'deleteSelectedCustomers'])
+</script>
---
+++ sec9/components/ContentsContainer.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex gap-8 p-5 bg-gray-900 antialiased">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+</script>
