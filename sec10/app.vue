a2ea53abe5aa224e040bb4bfbcda4242c1960c6d
haruocode <takaron0930@gmail.com>
2024-02-10 06:06:10 +0900

    sec10 コンポーネント名のtypo修正

--- sec10/app.vue
+++ sec10/app.vue
@@ -11,7 +11,7 @@
         class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
         COUNT UP
       </button>
-      <MyConponent v-if="isVisible" :count="count" />
+      <MyComponent v-if="isVisible" :count="count" />
     </ContentsContainer>
   </div>
 </template>
