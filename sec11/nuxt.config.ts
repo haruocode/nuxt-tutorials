5950becb6eee0c7b37bb9ac03ce07d3aa500ca1c
haruocode <takaron1979@yahoo.co.jp>
2024-03-17 06:26:24 +0900

    sec11 不要なコードを削除

--- sec11/nuxt.config.ts
+++ sec11/nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
-  css: ['~/assets/css/main.css'],
+  modules: ['@pinia/nuxt'],
 })
