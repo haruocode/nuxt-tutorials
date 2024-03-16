aaf3302b0f4a459d7f01ab48772f93f2851a18b1
haruocode <takaron1979@yahoo.co.jp>
2024-03-17 06:30:54 +0900

    sec11 2画面作成

---
+++ sec11/pages/another.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <nuxt-link to="/">Home</nuxt-link> | Another page
+  </div>
+  <h2>Another page</h2>
+  <div>
+    count: {{ count }}
+  </div>
+  <div>
+    <button @click="increment()">increment</button>
+  </div>
+</template>
+
+<script setup>
+
+const count = 999
+
+function increment() {}
+
+</script>
+
+<style></style>
---
+++ sec11/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    Home | <nuxt-link to="/another">Another page</nuxt-link>
+  </div>
+  <h2>Home</h2>
+  <div>
+    count: {{ store.count }}
+  </div>
+  <div>
+    <button @click="store.increment()">increment</button>
+  </div>
+</template>
+
+<script setup>
+
+const store = {
+  count: 999,
+  increment: () => {}
+}
+
+</script>
+
+<style></style>
