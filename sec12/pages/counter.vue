859ab43548c491194b8fa128759b442086838598
haruocode <takaron1979@yahoo.co.jp>
2024-03-20 21:02:44 +0900

    sec12 ready

---
+++ sec12/pages/counter.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2>Counter</h2>
+  <div>
+    count: {{ count }}
+  </div>
+  <div>
+    <button @click="increment()">increment</button>
+  </div>
+</template>
+
+<script setup>
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+
+</script>
+
+<style></style>
---
+++ sec12/pages/mouse.vue
@@ -0,0 +1,24 @@
+<template>
+  <h2>Mouse</h2>
+  <div>
+    x: {{ x }}<br>
+    y: {{ y }}
+  </div>
+</template>
+
+<script setup>
+
+const x = ref(0)
+const y = ref(0)
+
+function update(event) {
+  x.value = event.pageX
+  y.value = event.pageY
+}
+
+onMounted(() => window.addEventListener('mousemove', update))
+onUnmounted(() => window.removeEventListener('mousemove', update))
+
+</script>
+
+<style></style>
