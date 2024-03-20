f42ccd73e203182d3bac68d0f5d0728e30ef8598
haruocode <takaron1979@yahoo.co.jp>
2024-03-20 20:04:04 +0900

    sec12 ready

---
+++ sec12/pages/another.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <nuxt-link to="/">Home</nuxt-link> | Another page
+  </div>
+  <h2>Another page</h2>
+  <div>
+    count: 999
+  </div>
+  <div>
+    <button>increment</button>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style></style>
---
+++ sec12/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Home | <nuxt-link to="/another">Another page</nuxt-link>
+  </div>
+  <h2>Home</h2>
+  <div>
+    count: 999
+  </div>
+  <div>
+    <button>increment</button>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style></style>
