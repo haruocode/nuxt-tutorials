6521cff7cb4a79cc899b73ecaf2884dca756d398
haruocode <takaron1979@yahoo.co.jp>
2024-03-26 02:50:29 +0900

    sec14 add top page & auth page

---
+++ sec14/pages/auth.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="w-full h-[100vh] bg-white">
+    <div class="w-full flex items-center justify-center p-5 border-b border-b-gray-300">
+      <NuxtLink to="/">Supabase Auth Test</NuxtLink>
+    </div>
+
+    <div class="max-w-[400px] mx-auto px-2">
+      <div class="text-center my-6">Login</div>
+      <div>
+        <button @click="login('google')" type="button"
+          class="flex items-center justify-center w-full text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 shadow-green-500/50 font-medium rounded-lg py-2.5 text-center">
+          Login with Google
+        </button>
+        <br>
+        <button @click="login('github')" type="button"
+          class="flex items-center justify-center w-full text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg py-2.5 text-center">
+          Login with Github
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+function login() {
+  // some code comes here...
+}
+
+</script>
---
+++ sec14/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="w-full h-[100vh] bg-white">
+    <div class="w-full flex items-center justify-center p-5 border-b border-b-gray-300">
+      <NuxtLink to="/">Supabase Auth Test</NuxtLink>
+    </div>
+
+    <div class="max-w-[400px] mx-auto px-2">
+      <div class="text-center my-6">Top page</div>
+      <div class="text-center">
+        <NuxtLink to="/auth" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+          Go to Auth Page
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
