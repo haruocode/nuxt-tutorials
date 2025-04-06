1712d684047be58fa7137f2785868371ca41e78c
haruocode <takaron1979@yahoo.co.jp>
2025-04-06 22:04:33 +0900

    ch15 add トップ画面

--- sec15/pages/index.vue
+++ sec15/pages/index.vue
@@ -1,22 +1,38 @@
 <template>
-  <div class="flex flex-col items-center justify-center min-h-screen">
-    <h1 class="text-2xl font-bold mb-6">トップページ</h1>
-    <nuxtLink
-      class="bg-indigo-600 text-white px-6 py-2 rounded hover:bg-indigo-700"
-      to="/login"
-    >
-      ログイン画面へ
-    </nuxtLink>
+  <div class="flex flex-col items-center justify-center min-h-screen p-4">
+    <h1 class="text-3xl font-bold mb-6">WishWay クローンへようこそ！</h1>
+
+    <div v-if="!user">
+      <NuxtLink
+        to="/login"
+        class="bg-indigo-600 text-white px-6 py-2 rounded hover:bg-indigo-700"
+      >
+        GitHubでログインする
+      </NuxtLink>
+    </div>
+
+    <div v-else>
+      <p class="text-gray-600 mb-4">すでにログイン済みです。</p>
+      <NuxtLink
+        to="/my-wishlist"
+        class="bg-green-600 text-white px-6 py-2 rounded hover:bg-green-700"
+      >
+        マイ願いリストに移動
+      </NuxtLink>
+    </div>
   </div>
 </template>
 
 <script setup>
-const user = useSupabaseUser()
-const { auth } = useSupabaseClient()
+import { onMounted } from 'vue'
 
-watchEffect(() => {
+const supabase = useSupabaseClient()
+const user = useSupabaseUser()
+const router = useRouter()
+
+onMounted(() => {
   if (user.value) {
-    navigateTo('/my-wishlist')
+    router.push('/my-wishlist')
   }
 })
 </script>
