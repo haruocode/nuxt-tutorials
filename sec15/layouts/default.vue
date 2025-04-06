e37d8a0b1c46a6b128d3f85f508271c29f00f967
haruocode <takaron1979@yahoo.co.jp>
2025-04-06 22:56:03 +0900

    ch15 add ダークモードの準備

---
+++ sec15/layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :class="{ dark: isDark }" class="min-h-screen">
+    <main class="">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const isDark = ref(false)
+
+// 最初からユーザーのシステム設定を反映するなら
+// if (process.client) {
+//   isDark.value = window.matchMedia('(prefers-color-scheme: dark)').matches
+// }
+</script>
