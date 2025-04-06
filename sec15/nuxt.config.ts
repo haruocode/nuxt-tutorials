384a5b65f5f5c3f8dbab280889364a57b5549e5b
haruocode <takaron1979@yahoo.co.jp>
2025-04-06 13:53:51 +0900

    ch15 update nuxt.config.ts

--- sec15/nuxt.config.ts
+++ sec15/nuxt.config.ts
@@ -1,5 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/supabase',
+    '@nuxt/ui'
+  ],
+  supabase: {
+    // Supabaseの設定
+    redirect: {
+      login: '/login',
+      callback: '/confirm',
+      home: '/'
+    }
+  },
+  runtimeConfig: {
+    // 環境変数を設定
+    public: {
+      siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'http://localhost:3000'
+    }
+  }
 })
