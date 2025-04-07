41153d7a6eaaba80d179040cf199bc5a7fbbb2f7
haruocode <takaron1979@yahoo.co.jp>
2025-04-07 10:21:26 +0900

    ch15 update dev toolを非表示

--- sec15/nuxt.config.ts
+++ sec15/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   ssr: false,
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/supabase',
