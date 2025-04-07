f402bac049bfa97a73f11030e0a8ec827abc1eda
haruocode <takaron1979@yahoo.co.jp>
2025-04-07 10:21:08 +0900

    ch15 add フッター

--- sec15/layouts/default.vue
+++ sec15/layouts/default.vue
@@ -3,6 +3,10 @@
     <main class="">
       <slot />
     </main>
+
+    <footer class="bg-gray-200 dark:bg-gray-800 text-gray-600 dark:text-gray-300 text-center py-4 text-sm">
+      © 2025 WishWay Clone. No rights reserved at all.
+    </footer>
   </div>
 </template>
 
