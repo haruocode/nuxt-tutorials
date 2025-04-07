32c7bb0b5f4e41bb0eb8228a97a3f8beec02652c
haruocode <takaron1979@yahoo.co.jp>
2025-04-07 10:45:27 +0900

    ch15 update 認証リダイレクト無効

--- sec15/nuxt.config.ts
+++ sec15/nuxt.config.ts
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
     },
   },
   supabase: {
-    redirect: true,
+    redirect: false,
     redirectOptions: {
       login: '/login',
       callback: '/confirm',
